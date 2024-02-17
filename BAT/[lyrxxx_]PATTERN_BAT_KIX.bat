18b4836a829a746e6ee18d918b620339312a4e3f
lisitsinyr <lisitsinyr@gmail.com>
2024-02-17 15:46:31 +0400

    20240217154630

--- BAT/[lyrxxx_]PATTERN_BAT_KIX.bat
+++ BAT/[lyrxxx_]PATTERN_BAT_KIX.bat
@@ -2,72 +2,100 @@
 rem -------------------------------------------------------------------
 rem [lyrxxx_]PATTERN_BAT.bat
 rem -------------------------------------------------------------------
-chcp 1251
+chcp 1251>NUL
 
-rem echo -------------------------------------------------------
-rem Файл скрипта: каталог+имя+расширение
-set SCRIPT_FULLFILENAME=%~f0
+setlocal enabledelayedexpansion
+rem setlocal enableextensions disabledelayedexpansion
+echo ERRORLEVEL: %ERRORLEVEL%
+
+rem SCRIPT_FULLFILENAME - Файл скрипта [каталог+имя+расширение]
+set SCRIPT_FULLFILENAME=%0
 rem echo SCRIPT_FULLFILENAME: %SCRIPT_FULLFILENAME%
-rem Файл скрипта: имя+расширение
-set SCRIPT_BASEFILENAME=%~n0%~x0
-rem echo SCRIPT_BASEFILENAME: %SCRIPT_BASEFILENAME%
-rem Файл скрипта: имя
-set SCRIPT_FILENAME=%~n0
-rem echo SCRIPT_FILENAME: %SCRIPT_FILENAME%
-rem Каталог BAT_DIR: каталог
-if "%BAT_DIR%"=="" (
-    set BAT_DIR=D:\TOOLS\TOOLS_BAT\BAT
-    set BAT_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT\BAT
+rem SCRIPTS_DIR - Каталог скриптов BAT
+if "%SCRIPTS_DIR%" == "" (
+    set SCRIPTS_DIR=D:\TOOLS\TOOLS_BAT
+    set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT
+    set SCRIPTS_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\PROJECTS_BAT\TOOLS_BAT
 )
-rem echo BAT_DIR: %BAT_DIR%
-rem -------------------------------------------------------------------
-rem APP_KIX_DIR - каталог APP_KIX
-set APP_KIX_DIR=
-rem -------------------------------------------------------------------
-rem APP_KIX - Скрипт APP_KIX имя+расширение
-set APP_KIX=[lyrxxx_]PATTERN_KIX.kix
-rem -------------------------------------------------------------------
+rem echo SCRIPTS_DIR: %SCRIPTS_DIR%
+rem SCRIPTS_DIR_KIX - Каталог скриптов KIX
+if "%SCRIPTS_DIR_KIX%" == "" (
+    set SCRIPTS_DIR_KIX=D:\TOOLS\TOOLS_KIX
+    set SCRIPTS_DIR_KIX=G:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\TOOLS_KIX
+    set SCRIPTS_DIR_KIX=G:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\01_KIX\PROJECTS_KIX\TOOLS_KIX
+)
+rem echo SCRIPTS_DIR_KIX: %SCRIPTS_DIR_KIX%
 
-call %BAT_DIR%\__SET__.bat
-call %BAT_DIR%\__SET_KIX__.bat
+echo ==================================================================
+echo SET %SCRIPT_FULLFILENAME% ...
+echo ==================================================================
+call :__SET_VAR_SCRIPT %SCRIPT_FULLFILENAME% || exit /b 1
+call :__SET_VAR_DEFAULT || exit /b 1
+call :__SET_VAR_PROJECTS || exit /b 1
+call :__SET_CHECK_REPO || exit /b 1
+call :__SET_LOG || exit /b 1
+call :__SET_KIX || exit /b 1
 
 :begin
-echo ------------------------------------------------------- >> %LOG_FULLFILENAME%
-echo Start %SCRIPT_BASEFILENAME% ...       >> %LOG_FULLFILENAME%
-echo ------------------------------------------------------- >> %LOG_FULLFILENAME%
-echo CURRENT_DIR: %CURRENT_DIR%            >> %LOG_FULLFILENAME%
+call :StartLogFile || exit /b 1
+
+echo ================================================================= >> %LOG_FULLFILENAME%
+echo START %SCRIPT_BASEFILENAME% ... >> %LOG_FULLFILENAME%
+echo ================================================================== >> %LOG_FULLFILENAME%
 set DIR_SAVE=%CURRENT_DIR%
 
-call :Check_P1 || exit /b 1
+call :MAIN || exit /b 1
 
-if exist %APP_KIX_DIR%\%APP_KIX% (
-    echo START script %APP_KIX_DIR%\%APP_KIX% ... >> %LOG_FULLFILENAME%
-    kix32.exe %APP_KIX_DIR%\%APP_KIX% "$P1=%1" "$P2=%2" "$P3=%3" "$P4=%4" "$P5=%5" "$P6=%6" "$P7=%7" "$P8=%8" "$P9=%9"
-)
-
-rem far -v %LOG_FULLFILENAME%
+echo ================================================================= >> %LOG_FULLFILENAME%
+echo STOP %SCRIPT_BASEFILENAME% ... >> %LOG_FULLFILENAME%
+echo ================================================================== >> %LOG_FULLFILENAME%
 
 cd /D %DIR_SAVE%
+rem far -v %LOG_FULLFILENAME%
+
+call :StopLogFile || exit /b 1
 
 rem Выход из сценария. Дальше - только функции.
 :Exit
 exit /b 0
 
 rem =================================================
-rem ФУНКЦИИ
+rem ФУНКЦИИ LIB
 rem =================================================
-:Check_P1
-echo ---------------------------------------------------------------
-echo Check_P1 [P1]
-echo ---------------------------------------------------------------
-if "%1"=="" (
-    rem set /p P1=P1:
-    set P1=""
-) else (
-    set P1=%1
-)
-if "%P1%"=="" (
-    set P1=""
-)
-echo P1: %P1%
+:__SET_VAR_SCRIPT
+%SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
+:__SET_VAR_DEFAULT
+%SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
 exit /b 0
+:__SET_VAR_PROJECTS
+%SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
+exit /b 0
+:__SET_CHECK_REPO
+%SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
+exit /b 0
+:__SET_LOG
+%SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
+:__SET_KIX
+%SCRIPTS_DIR%\LIB\__SET_LIB.bat %*
+exit /b 0
+:StartLogFile
+%SCRIPTS_DIR%\LIB\LYRLog.bat %*
+exit /b 0
+:StopLogFile
+%SCRIPTS_DIR%\LIB\LYRLog.bat %*
+exit /b 0
+:Check_P
+%SCRIPTS_DIR%\LIB\LYRSupport.bat %*
+exit /b 0
+
+rem =================================================
+rem ФУНКЦИЯ :MAIN
+rem =================================================
+:MAIN
+rem beginfunction
+    if exist %APP_KIX_DIR%\%APP_KIX% (
+        echo START script %APP_KIX_DIR%\%APP_KIX% ... >> %LOG_FULLFILENAME%
+        kix32.exe %APP_KIX_DIR%\%APP_KIX% "$P1=%1" "$P2=%2" "$P3=%3" "$P4=%4" "$P5=%5" "$P6=%6" "$P7=%7" "$P8=%8" "$P9=%9"
+    )
+    exit /b 0
+rem endfunction
