81adf4047a364419e95c223a3a90eb443980f5ad
lisitsinyr <lisitsinyr@gmail.com>
2024-02-17 18:44:23 +0400

    20240217184423

--- BAT/[lyrxxx_]PATTERN_BAT_KIX.bat
+++ BAT/[lyrxxx_]PATTERN_BAT_KIX.bat
@@ -6,7 +6,15 @@ chcp 1251>NUL
 
 setlocal enabledelayedexpansion
 rem setlocal enableextensions disabledelayedexpansion
-echo ERRORLEVEL: %ERRORLEVEL%
+rem echo ERRORLEVEL: %ERRORLEVEL%
+
+rem PROJECTS - проект
+set PROJECTS=PROJECTS_BAT
+rem echo PROJECTS: %PROJECTS%
+set PROJECTS_LYR_DIR=D:\PROJECTS_LYR
+rem echo PROJECTS_LYR_DIR: %PROJECTS_LYR_DIR%
+set PROJECTS_DIR=%PROJECTS_LYR_DIR%\CHECK_LIST\03_SCRIPT\04_BAT\%PROJECTS%
+rem echo PROJECTS_DIR: %PROJECTS_DIR%
 
 rem SCRIPT_FULLFILENAME - Файл скрипта [каталог+имя+расширение]
 set SCRIPT_FULLFILENAME=%0
